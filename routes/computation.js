fa1b2a482aeda8973fb7ef12e5123de99ed2138b
nannehussain <143141256+nannehussain@users.noreply.github.com>
2023-10-25 15:23:46 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // Define the "computation" route
-router.get('/computation', function(req, res, next) {
+router.get('/', function(req, res, next) {
   // Get the value of the query parameter "x" or generate a random number if not provided
   var x = parseFloat(req.query.x) || (Math.random() * 10); // Generates a random number between 0 and 10
 
